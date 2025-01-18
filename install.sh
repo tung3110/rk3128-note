7289406b6add6279eed1398f7cd957de3490b8a7
tung3110 <tungnh@outlook.com>
2025-01-18 11:14:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ unset HISTFILE
 set +o history
 export HISTSIZE=0
 export HISTFILESIZE=0
-apt install iputils-ping
-apt install alsa-utils
-apt-get install gcc-arm-linux-gnueabihf
+apt -y install iputils-ping
+apt -y install alsa-utils
+apt-get -y install gcc-arm-linux-gnueabihf
 apt -y install git && git clone https://github.com/tung3110/ffmpeg-arm-install.git && bash ./ffmpeg-arm-install/install-armv71.sh
-git clone https://github.com/tung3110/rk3128-note.git
-cd rk3128-note
+#git clone https://github.com/tung3110/rk3128-note.git
+#cd rk3128-note
 unzip audioip.zip
 arm-linux-gnueabihf-gcc  *.c -c --static
 arm-linux-gnueabihf-ar rcs libPahoMqttC.a *.o
